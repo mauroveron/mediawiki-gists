314fbc5ae49ce33941950ef51a2e113e988e7b01
Mauro Verón <mauroveron@gmail.com>
2012-08-11 23:18:10 -0300

    Add gist extension.

---
+++ gists.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Syntax highlighting extension for MediaWiki 1.5 using GeSHi
+ * Copyright (C) 2005 Brion Vibber <brion@pobox.com>
+ * http://www.mediawiki.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+ * @file
+ * @ingroup Extensions
+ * @author Mauro Veron <opensource@mauroveron.com>
+ *
+ * This extension allows embedding of Github's gists using the <gist> tag
+ * like so: <gist>12345678</gist>.
+ *
+ */
+
+$wgExtensionCredits['gists'][] = array(
+  'path' => __FILE__,
+  'name' => 'Gists',
+  'author' => 'Mauro Veron',
+  'url' => '',
+  'description' => 'Show embedded gists on your wiki.',
+  'version' => 1.0,
+);
+
+$wgHooks['ParserFirstCallInit'][] = 'mvGists';
+
+function mvGists(Parser $parser)
+{
+  $parser->setHook('gist', 'mvGistRender');
+  return true;
+}
+
+/**
+ * Parses $input (gist number) and embeds gist code.
+ */
+function mvGistRender($input, $args, $parser, $frame)
+{
+  if (preg_match('/^\s*[0-9]+\s*$/', $input)) {
+    return '<script src="https://gist.github.com/'.trim($input).'.js"> </script>';
+  } else {
+    return '!!! Invalid gist number';
+  }
+}
